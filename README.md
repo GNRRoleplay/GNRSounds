2eeea63ca3b9e18b1b00126935bd997d524949f8
WesternHeights <63084492+Harraa@users.noreply.github.com>
2023-02-24 17:23:35 +0000

    add custom vehicle sounds

--- README.md
+++
@@ -1,2 +0,0 @@
-# GNRSounds
-Vehicle Sounds
---
+++ fxmanifest.lua
@@ -0,0 +1,64 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/*.dat10.rel',
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat54.rel',
+  'sfx/**/*.awc'
+}
+
+-- GOLFR Sound
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ea888_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ea888_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ea888'
+-- EVO 10 Sound
+data_file 'AUDIO_GAMEDATA' 'audioconfig/n4g63t_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/n4g63t_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_n4g63t'
+-- Z4/A90 Souund
+data_file 'AUDIO_GAMEDATA' 'audioconfig/b58b30_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/b58b30_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_b58b30'
+-- Subaru Sound
+data_file 'AUDIO_GAMEDATA' 'audioconfig/subaruej20_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/subaruej20_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_subaruej20'
+-- BMW M2 Sound
+data_file 'AUDIO_GAMEDATA' 'audioconfig/s55b30_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/s55b30_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_s55b30'
+-- BMW M3/4 Sound
+data_file 'AUDIO_GAMEDATA' 'audioconfig/n55b30t0_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/n55b30t0_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_n55b30t0'
+-- RS4 Sound
+data_file "AUDIO_SYNTHDATA" "audioconfig/audirs4milltekeng_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/audirs4milltekeng_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/audirs4milltekeng_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_audirs4milltekeng"
+-- RS6 Sound
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ea825_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ea825_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ea825'
+--Skyline R34 Sound
+data_file 'AUDIO_GAMEDATA' 'audioconfig/rb26dett_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/rb26dett_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_rb26dett'
+-- RX7 Sound
+data_file 'AUDIO_GAMEDATA' 'audioconfig/aq31maz13btune_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/aq31maz13btune_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aq31maz13btune'
+-- Evo 7/8/9 Sound
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ta011mit4g63_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta011mit4g63_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta011mit4g63'
+-- VW-Audi V8 4.0T Sound
+data_file 'AUDIO_GAMEDATA' 'audioconfig/taaud40v8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/taaud40v8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_taaud40v8'
+-- Brabus Sound
+data_file 'AUDIO_GAMEDATA' 'audioconfig/mercm177_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/mercm177_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_mercm177'
